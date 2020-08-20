9d44a245305d33552b7055de9c9c417a3625dadd
Ynda Jas <yndajas@gmail.com>
2020-08-20 19:44:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,13 +2,15 @@ class Triangle
   attr_accessor :a, :b, :c
   
   def initialize(a,b,c)
-    
+    @a = a
+    @b = b
+    @c = c
   end
   
   def kind
     type = ""
     
-    if a == b && b == c
+    if @a == @b && @b == @c
       :equilateral
     elsif
       
